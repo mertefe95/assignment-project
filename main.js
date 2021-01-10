f14cc0138fe0c5f0817d69b873eca66cfc9aa8d1
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-01-10 20:42:09 +0300

    new commit

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
   spaceBetween: 30,
   slidesPerGroup: 1,
-  slidesPerGroupSkip: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
@@ -10,36 +9,100 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
   navigation: {
-    nextEl: '.fa-long-arrow-alt-right',
+    nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
 
-
-let arr;
-let kek;
-
-let model1 = document.querySelector(".one-cikan-model-1").textContent;
-
-let model2 = document.querySelector(".one-cikan-model-2")
-let model3 = document.querySelector(".one-cikan-model-3")
-let model4 = document.querySelector(".one-cikan-model-4")
+var mySwiper = new Swiper('.swiper-two', {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
 
 
-console.log(model1)
-
-fetch('/data-json/bestSeller.json')
-  .then(response => response.json())
-  .then(data => document.querySelector(".one-cikan-model-1").textContent = data[0].title)
-  .catch(err => console.log(err));
-
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-new',
+    clickable: true
+  },
   
 
-  console.log(model1)
-  
-  fetch('/data-json/relatedProducts.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(error));
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-new',
+    prevEl: '.swiper-button-prev-new',
+  },
 
-  
+ 
+})
+
+
+mySwiper.update();
+
+fetch("./data-json/relatedProducts.json").then(
+  res =>{ 
+    res.json().then(
+      data=>{
+        
+        if(data.length > 0) {
+          var temp = "";
+
+          data.forEach((u) => {
+            temp += "<div class='product-container'>";
+            temp += "<ul class='ul-item'>";
+            temp += `<li class="li-item li-img"><img src="${u.img}" class="u-img" /></li>`; 
+            temp += `<li class="li-item li-star-comment"><span class="star-span"><i class="fas fa-star"></i> ${u.rating}</span> <span class="u-comment">(${u.comment} Yorum)</span></li>`;
+            temp += `<li class="li-item li-code"><p class="u-code">${u.code}</p></li>`;
+            temp += `<li class="li-item li-title"><p class="u-title">${u.title}</p></li>`; 
+            temp += `<li class="li-item li-price"><i class="fas fa-lira-sign"></i><span class="u-price"> ${u.price}</span></li>`;
+            temp += `<li class="li-item li-kargo"><span class="span-kargo">BUGÜN KARGODA</span></li>`;
+            temp += `<li class="li-item li-button-blue"><button type='submit' class='sepet-btn'><i class="fas fa-exchange-alt"></i><span class="span-sepet">SEPETE EKLE</span> </button></li>` 
+            temp += "</ul>";
+            temp += "</div>";
+          })
+          // close for loop
+
+          document.getElementById("data-first").innerHTML = temp;
+        }
+      }
+    )
+  }
+)
+
+
+fetch("./data-json/bestSeller.json").then(
+  res =>{ 
+    res.json().then(
+      data=>{
+        
+        if(data.length > 0) {
+          var temp = "";
+
+
+          data.forEach((u) => {
+            temp += "<div class='swiper-slide'>";
+            temp += "<ul class='ul-item'>";
+            temp += `<li class="li-item li-img"><img src="${u.img}" class="u-img" /></li>`; 
+            temp += `<li class="li-item li-star-comment"><span class="star-span"><i class="fas fa-star"></i> ${u.rating}</span> <span class="u-comment">(${u.comment} Yorum)</span></li>`;
+            temp += `<li class="li-item li-code"><p class="u-code">${u.code}</p></li>`;
+            temp += `<li class="li-item li-title"><p class="u-title">${u.title}</p></li>`; 
+            temp += `<li class="li-item li-price"><i class="fas fa-lira-sign"></i><span class="u-price"> ${u.price}</span></li>`;
+            temp += `<li class="li-item li-kargo"><span class="span-kargo">BUGÜN KARGODA</span></li>`;
+            temp += `<li class="li-item li-button-blue"><button type='submit' class='sepet-btn'><i class="fas fa-exchange-alt"></i><span class="span-sepet">SEPETE EKLE</span> </button></li>` 
+            temp += "</ul>";
+            temp += "</div>";
+          })
+        
+
+          document.getElementById("cok-satanlar-id").innerHTML = temp;
+        }
+      }
+    )
+  }
+)
+
+
+
